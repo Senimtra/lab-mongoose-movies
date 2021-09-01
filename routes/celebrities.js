f17eba92b5193022586505708fc225d93749aba1
Senimtra <senimtra@hotmail.com>
2021-09-01 15:47:20 +0200

    solve iteration 4: adding new celebrities

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -22,6 +22,25 @@ celebrityRouter.get('/', (req, res, next) => {
       });
 });
 
+// ### Route handler create celebrity form ###
+celebrityRouter.get('/create', (req, res) => {
+   res.render('celebrities/create');
+});
+
+// ### Route handler create celebrity submit ###
+celebrityRouter.post('/', (req, res) => {
+   const { name, occupation, catchPhrase } = req.body;
+   const newCelebrity = new Celebrity({ name, occupation, catchPhrase });
+   newCelebrity
+      .save()
+      .then(() => {
+         res.redirect('/celebrities');
+      })
+      .catch(error => {
+         res.render('celebrities/create');
+      });
+});
+
 // ### Route handler celebrity details ###
 celebrityRouter.get('/:id', (req, res, next) => {
    const celebrityId = req.params.id;
